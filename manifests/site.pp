a36f9197d4b056579bce445bc3799850ad5a4aed
Elexia12 <xiannie.rivas@gmail.com>
2016-03-28 15:52:23 -0600

    Fixing issues

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  file { '/etc/motd'
+  file { '/etc/motd':
     ensure  =>  file,
     owner   =>  'root',
     group   =>  'root',
