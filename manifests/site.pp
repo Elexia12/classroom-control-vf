feb2625a947077521615eefc84ced5659b9c5c81
Elexia12 <xiannie.rivas@gmail.com>
2016-03-29 10:26:06 -0600

    Include users

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  
+  include users
+  
   file { '/etc/motd':
     ensure  =>  file,
     owner   =>  'root',
