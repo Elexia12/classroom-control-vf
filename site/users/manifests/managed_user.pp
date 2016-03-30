2fb599dfb8f57b0e212446b1f1c8520fe16829d3
Elexia12 <xiannie.rivas@gmail.com>
2016-03-30 15:43:18 -0600

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,12 +1,28 @@
-define users::managed_user (
-  $group = $title,
-) {
-    user { $title:
-    ensure => present,
+define users::managed_users(
+  $username   = $title,
+  $uid        = undef,
+  $groupname  = undef,
+  $homedir    = "/home/${title}",
+){
+  File {
+    owner => $username,
+    group => $groupname,
+    mode  => '0644' # For directories will automatically go to 0755
   }
-  file { "/home/${title}":
-    ensure => directory,
-    owner => $title,
-    group => $group,
+  
+  user { $username:
+    ensure  => present,
+    uid     => $uid,
+    gid     => $groupname,
+    home    => $homedir,
   }
+  
+  group { $groupname:
+    ensure  => present,
+  }
+  
+  file { [$homedir, "${homedir}/.ssh"]:
+    ensure => directory
+  }
+  
 }
