90bb99f15e50b916da44ad60ffcb8e8a9f43996b
Elexia12 <xiannie.rivas@gmail.com>
2016-03-30 15:06:42 -0600

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,11 +1,11 @@
 class users::admins {
   
-  user::managed_user { 'Jose':}
+  users::managed_user { 'Jose':}
   
-  user::manage_user { 'Alice':
+  users::manage_user { 'Alice':
     group => 'admins',
   }
-  user::manage_user { 'Chen':
+  users::manage_user { 'Chen':
     group => 'admins',
   }
   
